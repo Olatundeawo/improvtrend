881c55a03c537e6e9c582964fab79139f83b06df
Babatunde <awotimilehin.babatunde@gmail.com>
2025-12-21 07:35:50 +0100

    added the logic

---
+++ backend/src/services/story.service.js
@@ -0,0 +1,37 @@
+import prisma from "../../prisma/client";
+
+export async function createStory(userId, { title, content}) {
+    return prisma.story.create({
+        data: {
+            title, content, userId
+        }
+    })
+}
+
+export async function getStories() {
+    return prisma.story.findMany({
+        include: {
+            turns: {
+                include: {upvotes: true}
+            }
+        },
+        orderBy: {createdAt: "desc" }
+    });
+}
+
+export async function getStoryById(id) {
+    return prisma.story.findUnique({
+        where: { id },
+        include: {
+            turns: {
+                include: {
+                    upvotes: true,
+                    character: true,
+                    user: { select: {username: true}}
+                },
+                orderBy: { createdAt: "asc" }
+            },
+            comments: true
+        }
+    })
+}
