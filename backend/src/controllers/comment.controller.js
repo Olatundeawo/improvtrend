cf971bb9fc5ae331232c66c7879625b4ddbc9279
Babatunde <awotimilehin.babatunde@gmail.com>
2025-12-22 08:49:04 +0100

    'modify'

--- backend/src/controllers/comment.controller.js
+++ backend/src/controllers/comment.controller.js
@@ -1,19 +1,29 @@
-import { addComment } from "../services/comment.service";
+import { addComment } from "../services/comment.service.js";
 
 export async function comment(req, res) {
-    try {
-        const storyId = req.params
-        const userId = req.user.id
-        const { content } = req.body
-        const maxChar = 100
+  try {
+    const { storyId } = req.params;
+    const userId = req.user.id;
+    const { content } = req.body;
 
-        if (content > maxChar) {
-            throw new Error("maximum coment characters is 100")
-        }
+    const MAX_CHAR = 100;
 
-        const comment = await addComment(storyId, userId, content)
-        res.status(201).json(comment)
-    } catch (err) {
-        res.status(400).json({ Error: err.message})
+    if (!content || content.trim().length === 0) {
+      throw new Error("Comment cannot be empty");
     }
+
+    if (content.length > MAX_CHAR) {
+      throw new Error("Maximum comment length is 100 characters");
+    }
+
+    const comment = await addComment(
+      storyId,
+      userId,
+      { content }
+    );
+
+    res.status(201).json(comment);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 }
