f6a44b7dc064d8c833d37eb6a1c8c9b0151c8431
Babatunde <awotimilehin.babatunde@gmail.com>
2025-12-22 08:52:14 +0100

    'modify'

--- backend/src/routes/story.routes.js
+++ backend/src/routes/story.routes.js
@@ -4,8 +4,8 @@ import { create, get, getAll } from "../controllers/story.controller"
 
 const router = express.Router()
 
-router.post("/create/story", auth, create)
-router.get("/:id/story", get)
-router.get("/stories", getAll)
+router.post("/", auth, create)
+router.get("/:id", get)
+router.get("/", getAll)
 
 export default router;
