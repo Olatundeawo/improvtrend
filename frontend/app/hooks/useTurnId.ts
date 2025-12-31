9e04b03f4ffadf473feb45fc0748e8d6ae8fcfa7
Babatunde <awotimilehin.babatunde@gmail.com>
2025-12-31 23:32:51 +0100

    added the turn id logic

---
+++ frontend/app/hooks/useTurnId.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { useLocalSearchParams } from "expo-router";
+import { useEffect, useState } from "react";
+
+export default function useTurnId() {
+  const { id } = useLocalSearchParams<{ id: string }>();
+  const [turn, setTurn] = useState<any>(null);
+  const [loading, setLoading] = useState(true);
+
+  const URL = process.env.EXPO_PUBLIC_BASE_URL;
+
+  useEffect(() => {
+    if (!id) return;
+
+    const getStory = async () => {
+      try {
+        const res = await axios.get(`${URL}${id}/turns/`);
+        setTurn(res.data);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getStory();
+  }, [id]);
+
+  return { turn, loading };
+}
