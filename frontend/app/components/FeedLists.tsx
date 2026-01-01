42afc5fa02ce88fda81c1447a4212c2f14986831
Babatunde <awotimilehin.babatunde@gmail.com>
2026-01-01 07:17:55 +0100

    make it fecth formattime

--- frontend/app/components/FeedLists.tsx
+++ frontend/app/components/FeedLists.tsx
@@ -8,36 +8,13 @@ import {
 } from "react-native";
 import FeedSkeleton from "./FeedSkeleton";
 import { Story } from "./type";
+import formatTime from '../hooks/time'
 
 type FeedListProps = {
   stories: Story[];
   onStoryPress: (id: string) => void;
   isLoading?: boolean;
 };
-
-
-function formatTime(date: string | Date) {
-  const created = new Date(date);
-  const diffSeconds = Math.floor(
-    (Date.now() - created.getTime()) / 1000
-  );
-
-  // Less than 24 hours → relative time
-  if (diffSeconds < 86400) {
-    if (diffSeconds < 60) return `${diffSeconds}s ago`;
-    const minutes = Math.floor(diffSeconds / 60);
-    if (minutes < 60) return `${minutes}m ago`;
-    const hours = Math.floor(minutes / 60);
-    return `${hours}h ago`;
-  }
-
-  // More than 24 hours → date format (Dec 29)
-  return created.toLocaleDateString(undefined, {
-    month: "short",
-    day: "numeric",
-  });
-}
-
 
 export default function FeedList({
   stories,
