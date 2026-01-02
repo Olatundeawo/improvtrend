3c2c92b4b0717e39a776b9b241a468fca80ca9b5
Babatunde <awotimilehin.babatunde@gmail.com>
2026-01-02 08:33:18 +0100

    modifty

--- frontend/app/hooks/useTurn.ts
+++ frontend/app/hooks/useTurn.ts
@@ -1,15 +1,20 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 import { useLocalSearchParams } from "expo-router";
+import { useState } from 'react'
 
 export default function useTurn() {
   const { id } = useLocalSearchParams();
   const URL = process.env.EXPO_PUBLIC_BASE_URL;
+  const [error, setError] = useState<string | null>(null)
+  const [message, setMessage] = useState<string | null>(null)
 
   const createTurn = async (
     characterId: string,
     content: string
   ) => {
+    setError(null)
+    setMessage(null)
     const token = await AsyncStorage.getItem("token");
 
     if (!token) {
@@ -30,14 +35,20 @@ export default function useTurn() {
       );
 
       if (res.status === 201) {
-        return res.data; 
+        setMessage("Successfuly added your contribution.")
       }
-    } catch (err) {
-      console.log("Error creating turn", err);
-    }
-
-    return null;
+    } catch (err: any) {
+        if (axios.isAxiosError(err)) {
+          setError(
+            err.response?.data?.error ||
+            "Something went wrong"
+          );
+        } else {
+          setError("Network error, check your internet connection");
+        }
+        return null;
+      }
   };
 
-  return { createTurn };
+  return { createTurn, error };
 }
