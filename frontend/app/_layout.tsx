7434485848203e492c109dea9beec31074e94d38
Babatunde <awotimilehin.babatunde@gmail.com>
2026-01-02 05:47:41 +0100

    added a font family

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,5 +1,32 @@
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import {
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold,
+} from "@expo-google-fonts/inter";
+
+import {
+  PlayfairDisplay_600SemiBold,
+} from "@expo-google-fonts/playfair-display";
+
 
 export default function RootLayout() {
-  return <Stack />;
+
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    PlayfairDisplay_600SemiBold,
+  });
+
+  if (!fontsLoaded) return null;
+ 
+  return (
+    <Stack screenOptions={{ headerShown: false}}>
+      <Stack.Screen name="(tabs)" />
+      </Stack>
+  );
 }
