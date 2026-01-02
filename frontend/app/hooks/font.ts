23b8713442633bcbb40eaabb525808750a58414d
Babatunde <awotimilehin.babatunde@gmail.com>
2026-01-02 05:48:09 +0100

    create a font family

---
+++ frontend/app/hooks/font.ts
@@ -0,0 +1,8 @@
+const FONT = {
+    regular: "Inter_400Regular",
+    medium: "Inter_500Medium",
+    semibold: "Inter_600SemiBold",
+    bold: "Inter_700Bold",
+    title: "PlayfairDisplay_600SemiBold",
+  };
+  
