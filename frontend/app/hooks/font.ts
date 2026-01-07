6110b99aef430f917990d762570cdf844496b3a7
Babatunde <awotimilehin.babatunde@gmail.com>
2026-01-07 18:06:46 +0100

    delete the font

--- frontend/app/hooks/font.ts
+++
@@ -1,8 +0,0 @@
-const FONT = {
-    regular: "Inter_400Regular",
-    medium: "Inter_500Medium",
-    semibold: "Inter_600SemiBold",
-    bold: "Inter_700Bold",
-    title: "PlayfairDisplay_600SemiBold",
-  };
-  
