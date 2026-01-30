8493c8b332d976b47de73541926812da925ad1a9
olatundeawo <awotimilehin.babatunde@gmail.com>
2026-01-29 23:22:52 -0800

    removed the badges selected from user

--- backend/src/services/story.service.js
+++ backend/src/services/story.service.js
@@ -94,7 +94,7 @@ export async function getStories({ page = 1, limit = 10 }) {
           },
         },
         user: {
-          select: { username: true, badge: true },
+          select: { username: true },
         },
         comments: {
           select: { id: true },
