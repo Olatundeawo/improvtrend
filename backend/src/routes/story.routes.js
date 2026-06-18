9261ff6e3f1f56a6e0a5653141a682a237c93aba
olatundeawo <awotimilehin.babatunde@gmail.com>
2026-06-18 14:10:06 +0100

    added to the story route

--- backend/src/routes/story.routes.js
+++ backend/src/routes/story.routes.js
@@ -1,13 +1,21 @@
-import express from "express"
-import { create, get, getAll, getUserStoryById } from "../controllers/story.controller.js"
-import auth from "../middleware/auth.js"
+import express from "express";
+import {
+  create,
+  get,
+  getAll,
+  getUserStoryById,
+  voteComplete,
+  complete,
+} from "../controllers/story.controller.js";
+import auth from "../middleware/auth.js";
 
+const router = express.Router();
 
-const router = express.Router()
-
-router.post("/", auth, create)
-router.get("/:id", get)
-router.get("/", getAll)
-router.get("/user/:userId", auth, getUserStoryById);
+router.post("/",               auth, create);
+router.get("/",                      getAll);
+router.get("/user/:userId",    auth, getUserStoryById);
+router.get("/:id",                   get);
+router.post("/:id/vote-complete", auth, voteComplete);
+router.post("/:id/complete",      auth, complete);
 
 export default router;
