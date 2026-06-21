8e51800459c02cd7efe6589f22db8a024a4f8a02
olatundeawo <awotimilehin.babatunde@gmail.com>
2026-06-21 20:24:55 +0100

    added a new route

--- backend/src/routes/xp.routes.js
+++ backend/src/routes/xp.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getXpSummary, getXpHistory } from "../controllers/xp.controller.js";
+import { getXpSummary, getXpHistory, checkAction } from "../controllers/xp.controller.js";
 import auth from "../middleware/auth.js";
 
 const router = Router();
 
 router.get("/:userId/summary", auth, getXpSummary);
 router.get("/:userId/history", auth, getXpHistory);
+router.get("/can-perform/:action", auth, checkAction);
 
 export default router;
