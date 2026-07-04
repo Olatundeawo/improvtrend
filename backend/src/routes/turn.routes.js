dc440646e18cd5a0fa02715415924c388c9ddb88
olatundeawo <awotimilehin.babatunde@gmail.com>
2026-07-04 10:41:46 +0100

    added the turn edit route

--- backend/src/routes/turn.routes.js
+++ backend/src/routes/turn.routes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { continueStory, getTurns } from "../controllers/turn.controller.js"
+import { continueStory, getTurns, editTurnController } from "../controllers/turn.controller.js"
 import auth from "../middleware/auth.js";
 
 
@@ -8,4 +8,5 @@ const router = express.Router();
 
 router.post("/:storyId/turns", auth, continueStory);
 router.get('/:storyId/turns', getTurns)
+router.patch('/:storyId/turns/:turnId', auth, editTurnController)
 export default router;
