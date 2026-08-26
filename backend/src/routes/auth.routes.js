5142c6a35727e7a0abbcecb0c4a693a25198deb1
olatundeawo <awotimilehin.babatunde@gmail.com>
2026-08-26 10:39:56 +0100

    update the route

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { register, login } from "../controllers/auth.controller.js";
+import { register, login, loginWithGoogle } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-
+router.post("/google", loginWithGoogle);
 
 export default router;
